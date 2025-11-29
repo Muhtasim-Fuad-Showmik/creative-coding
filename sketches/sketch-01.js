6ce9ecf31c415b3449f64514e881e346d58b5d4e
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-11-29 14:50:06 +0600

    refactor: convert hardcoded parameters to percentages to make our shape dynamic to defined dimensions

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -14,17 +14,23 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
     // Draw a grid of squares with randomized inset squares
-    const rectWidth = 60;
-    const rectHeight = 60;
-    const gap = 20;
+    const rectWidth = width * 0.1;
+    const rectHeight = height * 0.1;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const offset = width * 0.02;
+
     let x, y;
-    context.lineWidth = 4;
+    
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-          x = 100 + (rectWidth + gap) * i;
-          y = 100 + (rectHeight + gap) * j;
+          x = ix + (rectWidth + gap) * i;
+          y = iy + (rectHeight + gap) * j;
 
           context.beginPath();
           context.rect(x, y, rectWidth, rectHeight);
@@ -34,7 +40,7 @@ const sketch = () => {
           if (Math.random() > 0.5) {
               // Create smaller rectangles inset within the larger ones
               context.beginPath();
-              context.rect(x + 8, y + 8, rectWidth - 16, rectHeight - 16);
+              context.rect(x + offset / 2, y + offset / 2, rectWidth - offset, rectHeight - offset);
               context.stroke();
           }
       }
