738b7417a2a90345635d5099621db5f7558a38d5
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-11-29 14:44:21 +0600

    feat: import our grid of square onto the sketch

--- sketches/README.md
+++ sketches/README.md
@@ -18,4 +18,10 @@ To create a new canvas sketch, run the following command:
 canvas-sketch sketch-01.js --new
 ```
 
+Canvas outputs can be saved directly. To define the output directory of images for your canvas sketch, run the following command (where output/01 has been used as an example directory of "01" within an "output" folder within the current project working directory):
+
+```[bash]
+canvas-sketch sketch-01.js --output=output/01
+```
+
 > **Note:** For more information visit the official [docs for canvas sketch](https://github.com/mattdesl/canvas-sketch/blob/master/docs/installation.md).
--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,7 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ] // Creates a square canvas with dimensions 2048 x 2048 pixels
+  dimensions: [ 600, 600 ] // Creates a square canvas with dimensions 2048 x 2048 pixels
 };
 
 const sketch = () => {
@@ -9,6 +9,31 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    // Draw a grid of squares with randomized inset squares
+    const rectWidth = 60;
+    const rectHeight = 60;
+    const gap = 20;
+    let x, y;
+    context.lineWidth = 4;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+          x = 100 + (rectWidth + gap) * i;
+          y = 100 + (rectHeight + gap) * j;
+
+          context.beginPath();
+          context.rect(x, y, rectWidth, rectHeight);
+          context.stroke();
+
+          // Randomize the drawing of inset rectangles
+          if (Math.random() > 0.5) {
+              // Create smaller rectangles inset within the larger ones
+              context.beginPath();
+              context.rect(x + 8, y + 8, rectWidth - 16, rectHeight - 16);
+              context.stroke();
+          }
+      }
+    }
   };
 };
 
