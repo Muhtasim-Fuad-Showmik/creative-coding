f797220b737bae06062874e538f0af03e1b427bd
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-11-29 16:22:10 +0600

    feat: generate 12 hands of the clock rotated around the perimeter using math formulae

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -10,28 +10,57 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'black'; // Set color of clock hands to black
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const rectWidth = width * 0.3;
-    const rectHeight = height * 0.3;
+    // Center of the canvas
+    const cx = width * 0.5;
+    const cy = height * 0.5;
 
-    // Save context to being a set of dedicated transformations
-    context.save();
+    // Width of clock hands
+    const handWidth = width * 0.01;
+    const handHeight = height * 0.1;
+    
+    let x, y; // Position of clock hands
 
-    // Translate and rotate the context
-    context.translate(x, y);
-    context.rotate(degToRad(45));
+    const numOfClockHands = 12; // Number of clock hands
+    const slice = degToRad(360/numOfClockHands); // Angle for each slice of the circle
+    const radius = width * 0.3; // Radius of the circle
 
-    // Draw the rectangle on the rotated context 
-    //(simliar to drawing on a rotated paper)
-    context.beginPath();
-    context.rect(-rectWidth * 0.5, -rectHeight * 0.5, rectWidth, rectHeight);
-    context.fill();
+    for (let i = 0; i < numOfClockHands; i++) {
+      const angle = slice * i; // Angle for each clock hand incremented in slices
 
-    // Restore the context to its original state
-    context.restore();
+      /**
+       * Calculate the (x, y) position for each clock hand using polar coordinates:
+       * 
+       * General formula for polar coordinates starting from,
+       * 0 degrees (3 o'clock horizontal axis) and clockwise rotation:
+       * Cartesian X coordinate = Center X + (Radius * cos(Angle))
+       * Cartesian Y coordinate = Center Y + (Radius * sin(Angle))
+       * 
+       * Implemented formula for polar coordinates starting from,
+       * 90 degrees (6 o'clock horizontal axis) and counter-clockwise rotation:
+       * Cartesian X coordinate = Center X + (Radius * sin(Angle))
+       * Cartesian Y coordinate = Center Y + (Radius * cos(Angle))
+       */
+      x = cx + Math.sin(angle) * radius;
+      y = cy + Math.cos(angle) * radius;
+
+      // Save context to being a set of dedicated transformations
+      context.save();
+      
+      // Translate and rotate the context
+      context.translate(x, y);
+      context.rotate(-angle);
+  
+      // Draw the rectangle on the rotated context 
+      //(simliar to drawing on a rotated paper)
+      context.beginPath();
+      context.rect(-handWidth * 0.5, -handHeight * 0.5, handWidth, handHeight);
+      context.fill();
+  
+      // Restore the context to its original state
+      context.restore();
+    }
 
     // Commented out the circle as it's no longer needed but can
     // help out as notes for the lesson it was meant for
