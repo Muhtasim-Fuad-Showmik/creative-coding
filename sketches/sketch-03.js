92b32f1962cec2e7060297f9a9abae829733e50a
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-12-13 10:47:10 +0600

    feat: use objects to draw two points instead

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -9,13 +9,41 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const point = { x: 900, y: 400, radius: 10 };
+    // Create instances of agents using only coordinates
+    const agentA = new Agent(800, 400);
+    const pointB = new Agent(300, 700);
 
-    context.beginPath();
-    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
-    context.fillStyle = "black";
-    context.fill();
+    // Use draw method from agent to draw the dots
+    agentA.draw(context);
+    pointB.draw(context);
   };
 };
 
 canvasSketch(sketch, settings);
+
+// Represents positional coordinates
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/**
+ * Preserves coordinates and defaults radius to 10 and then
+ * draws the dot on the canvas at the predefined position
+ */
+class Agent {
+  constructor(x, y) {
+    this.position = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.fillStyle = "black";
+
+    context.beginPath();
+    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
