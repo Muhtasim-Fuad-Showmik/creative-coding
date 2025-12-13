792a46e0e9ac086ee59ef971a1ffefff21eecf82
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-12-13 10:59:02 +0600

    feat: use translation instead of initial position; update visuals to be a stroked blank dot instead;

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -49,10 +49,16 @@ class Agent {
   }
 
   draw(context) {
-    context.fillStyle = "black";
+    context.save();
+    context.translate(this.position.x, this.position.y);
+
+    context.lineWidth = 4;
 
     context.beginPath();
-    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
